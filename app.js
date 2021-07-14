1965a9a79ec63d1cfb2274e009eb6341ac2b622f
jdelles <james.delles@gmail.com>
2021-07-13 22:02:58 -0400

    Added forms to index and rebuilt JS - need to figure out how to read form data in

--- app.js
+++ app.js
@@ -4,14 +4,14 @@
 let myLibrary = []; 
 
 /**
- * Gets the display id which is used to build the table of book info
- */
-const libraryDisplay = document.querySelector('#display'); 
-
-/**
  * Gets the addBook button which will be used by the user to add a new book to the library
  */
 const newBook = document.querySelector('#newBook'); 
+
+/**
+ * Gets the form to toggle on and off when newBook is clicked
+ */
+const addForm = document.querySelector('#form'); 
 
 /**
  * Tracks the size of the myLibrary array
@@ -25,87 +25,88 @@ let size = 0;
  * @param {*} pages the number of pages in the book
  * @param {*} read was the book read or not yet
  */
-function Book(title, author, pages, read) {
-    this.title = title;
-    this.author = author; 
-    this.pages = pages; 
-    this.read = read; 
-    this.index = size; 
+class Book {
+    constructor(title, author, pages, read) {
+        this.title = title;
+        this.author = author;
+        this.pages = pages;
+        this.read = read;
+        this.index = size;
 
-    /**
-     * Gets a string containing information about the book
-     * @returns a string containing information about the book
-     */
-    this.info = function() {
-        return `${title} by ${author}, ${pages}, ${read}`; 
+        /**
+         * Gets a string containing information about the book
+         * @returns a string containing information about the book
+         */
+        this.info = function () {
+            return `${title} by ${author}, ${pages}, ${read}`;
+        };
     }
 }
 
 /**
- * Adds a new book to the myLibrary array and updates the webpage display table with the new book
+ * Adds a new book to the library and updates the display table with the new book
  * @param {*} title the title of the book
  * @param {*} author the author of the book
  * @param {*} pages the number of pages in the book
  * @param {*} read was the book read or not yet
  */
 function addBookToLibrary(title, author, pages, read) {
-
-    /**
-     * Creates the new book object and adds it to the library
-     */
     let book = new Book(title, author, pages, read); 
     myLibrary.push(book);
     size++; 
+    libraryDisplay(); 
+}
 
-    /**
-     * Adds the book to the table on the website
-     */
-    const table = document.querySelector('table'); 
-    const row = table.insertRow(); 
-    const cell1 = row.insertCell(0);  
-    const cell2 = row.insertCell(1);
-    const cell3 = row.insertCell(2);
-    const cell4 = row.insertCell(3);
-    cell1.textContent = title; 
-    cell2.textContent = author; 
-    cell3.textContent = pages; 
-    cell4.textContent = read; 
+/**
+ * Removes a book from the library and updates the display table to remove the book
+ * @param {*} book the Book object to remove
+ */
+function removeBookFromLibrary(book) {
+    myLibrary.splice(myLibrary.findIndex(book), 1); 
+    size--; 
+    libraryDisplay(); 
+}
+
+/**
+ * Loops through each book in the libarry to build the display table
+ */
+function libraryDisplay() {
+    const tbody = document.querySelector('tbody'); 
+    tbody.innerHTML = ""; 
+    myLibrary.forEach(book => {
+        const row = document.createElement('tr'); 
+
+        const tdTitle = document.createElement('td'); 
+        tdTitle.textContent = book.title; 
+
+        const tdAuthor = document.createElement('td'); 
+        tdAuthor.textContent = book.author; 
+        
+        const tdPages = document.createElement('td'); 
+        tdPages.textContent = book.pages; 
+
+        const tdRead = document.createElement('td'); 
+        tdRead.textContent = book.read; 
+
+        row.appendChild(tdTitle); 
+        row.appendChild(tdAuthor); 
+        row.appendChild(tdPages); 
+        row.appendChild(tdRead); 
+        
+        tbody.appendChild(row); 
+    }); 
 }
 
 newBook.addEventListener('click', () => {
     const formSection = document.querySelector('#form'); 
-    const form = document.createElement('form');
-
-    const title = document.createElement('input'); 
-    title.setAttribute("type", "text"); 
-    title.setAttribute("name", "title");
-    title.setAttribute("placeholder", "title");  
-    
-    const author = document.createElement('input'); 
-    author.setAttribute("type", "text"); 
-    author.setAttribute("name", "author");
-    author.setAttribute("placeholder", "author");  
-    
-    const pages = document.createElement('input'); 
-    pages.setAttribute("type", "text"); 
-    pages.setAttribute("name", "pages");
-    pages.setAttribute("placeholder", "pages");  
-    
-    const read = document.createElement('input'); 
-    read.setAttribute("type", "text"); 
-    read.setAttribute("name", "title");
-    read.setAttribute("placeholder", "read");  
-
-    const submit = document.createElement('input'); 
-    submit.setAttribute("type", "submit"); 
-    submit.setAttribute("value", "submit"); 
-
-    form.appendChild(title);
-    form.appendChild(author);
-    form.appendChild(pages);
-    form.appendChild(read);
-    form.appendChild(submit); 
-    
-    form.setAttribute(onsubmit, addBookToLibrary(title, author, pages, read)); 
-    formSection.appendChild(form); 
+    formSection.style.display = "block";  
 });
+
+form.addEventListener("submit", (i) => {
+    const title = document.getElementById("title").nodeValue; 
+    const author = document.getElementById("author").nodeValue; 
+    const pages = document.getElementById("pages").nodeValue; 
+    const read = document.getElementById("read").nodeValue; 
+
+    addBookToLibrary(title, author, pages, read); 
+}); 
